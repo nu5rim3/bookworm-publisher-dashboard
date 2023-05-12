80d10ee5a886641965220c76211e5bfde5f33192
Nusri Samath <nusrisamath@gmail.com>
2023-05-13 04:15:33 +0530

    [Updated] - Added UI for post preview

---
+++ app/(main)/publisher/[id]/page.tsx
@@ -0,0 +1,14 @@
+export default function Preview() {
+    return
+    <>
+        <h1>hello</h1>
+    </>;
+}
+
+export async function getStaticPaths() {
+    // Return a list of possible value for id
+}
+
+export async function getStaticProps({ }) {
+    // Fetch necessary data for the blog post using params.id
+}
